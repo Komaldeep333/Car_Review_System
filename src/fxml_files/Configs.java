0259a20d22091611ee96f708c666184723cbe5a7
Komaldeep Kaur <komaldeep20017@gmail.com>
2025-07-20 17:22:00 +0530

    Add files via upload

---
+++ src/fxml_files/Configs.java
@@ -0,0 +1,9 @@
+package fxml_files;
+
+public class Configs {
+    protected static String host = "@localhost";
+    protected static String port = "1521";
+    protected static String database = "xe";
+    protected static String dbUser = "";
+    protected static String dbPass = "";
+}
